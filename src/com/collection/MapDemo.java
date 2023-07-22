69ade65e62849fd55e6c66592b13df87a6973052
admin <admin@DESKTOP-NL59TQ3>
2023-07-22 10:31:48 +0530

    ...

---
+++ src/com/collection/MapDemo.java
@@ -0,0 +1,27 @@
+package com.collection;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class MapDemo {
+	public static void main(String[] args) {
+		Map map = new HashMap();
+		map.put(101, "sameer");
+		map.put(102, "khan");
+		map.put(103, "aadil");
+		map.put(104, "siddiqui");
+
+		Set s = map.entrySet();
+
+		Iterator iterator = s.iterator();
+		while (iterator.hasNext()) {
+			Entry entry = (Entry) iterator.next();
+			System.out.println(entry);
+		}
+
+	}
+
+}
