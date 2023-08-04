127f33a5a4e2f0cb2a8a14c1fb756c6939b45864
admin <admin@DESKTOP-NL59TQ3>
2023-08-04 13:06:44 +0530

    ...

---
+++ src/com/collection/AddRemoveWhileIteratingUsingForEachLoop.java
@@ -0,0 +1,24 @@
+package com.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddRemoveWhileIteratingUsingForEachLoop {
+	public static void main(String[] args) {
+		List<String> fruits = new ArrayList<>();
+		fruits.add("Melons");
+		fruits.add("Mango");
+		fruits.add("Apple");
+		fruits.add("cherry");
+		fruits.add("Grapes");
+
+		System.out.println("Original Fruit list :- \n " + fruits + "\n");
+		for (String fruit : fruits) {
+			fruits.remove(fruit);
+			
+		}
+		 System.out.println("\nAfter removal of Melons & addition of Banana :- \n"
+	                + fruits);
+	}
+
+}
