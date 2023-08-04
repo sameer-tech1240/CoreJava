f7d13dc682249d3e7b458456ed0110a4beb79f9d
admin <admin@DESKTOP-NL59TQ3>
2023-08-04 23:03:05 +0530

    ...

---
+++ src/com/oops/OverrideMethod.java
@@ -0,0 +1,33 @@
+package com.oops;
+
+class A {
+	A() {
+		System.out.println("a");
+	}
+
+	void a1() {
+		System.out.println("a1");
+	}
+}
+
+class B extends A {
+	B() {
+		System.out.println("B");
+
+	}
+
+	void b1() {
+		System.out.println("B1");
+	}
+
+	void a1() {
+		System.out.println("A1");
+	}
+}
+
+public class OverrideMethod {
+	public static void main(String[] args) {
+		A a = new B();
+		a.a1();
+	}
+}
