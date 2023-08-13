e4322dd84a1b143716ff00865b28d92fd71de553
admin <admin@DESKTOP-NL59TQ3>
2023-08-13 11:54:06 +0530

    ...

---
+++ src/com/array/RightRotateArray.java
@@ -0,0 +1,27 @@
+package com.array;
+
+/*import javax.swing.JOptionPane;*/
+
+public class RightRotateArray {
+	public static void rotateArray(int[] n) {
+		int j = n.length - 1;
+		for (int i = 0; i < j; i++) {
+			int temp = n[i];
+			n[i] = n[j];
+			n[j] = temp;
+
+		}
+		for (int i = 1; i < n.length; i++) {
+			System.out.print(n[i]);
+		}
+
+		/*
+		 * JOptionPane.showConfirmDialog(null, "Fuck");
+		 */
+	}
+
+	public static void main(String[] args) {
+		int[] n = { 1, 2, 3, 4, 5, 6, 7, 8 };
+		rotateArray(n);
+	}
+}
--- src/com/array/ShortingArray.java
+++ src/com/array/ShortingArray.java
@@ -1,16 +1,23 @@
 package com.array;
 
+import java.util.Arrays;
+
 public class ShortingArray {
-	public static void shortingArray(int[] n) {
+	public static void shortArray(int[] n) {
 		for (int i = 0; i < n.length; i++) {
-			for (int j = 0; j < n.length; j++) {
-				if (n[j]>n[j+1]) {
-					
+			for (int j = 0; j < n.length - 1; j++) {
+				if (n[j] > n[j + 1]) {
+					int temp = n[j];
+					n[j] = n[j + 1];
+					n[j + 1] = temp;
 				}
 			}
 		}
+		System.out.println(Arrays.toString(n));
 	}
-public static void main(String[] args) {
-	
-}
+
+	public static void main(String[] args) {
+		int n[] = { 8, 4, 3, 1, 7, 6, 5, 2 };
+		shortArray(n);
+	}
 }
