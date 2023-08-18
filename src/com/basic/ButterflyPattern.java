5935405cd6bc4e5f3f87a95c39cd87e2552156db
sameersiddiqui1240 <sameersiddiqui1240@gamil.com>
2023-08-18 21:42:54 +0530

    ...

--- src/com/basic/ButterflyPattern.java
+++ src/com/basic/ButterflyPattern.java
@@ -1,8 +1,6 @@
 package com.basic;
-
 public class ButterflyPattern {
 	public static void main(String[] args) {
-
 		int n = 4;
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j <= i; j++) {
@@ -16,8 +14,7 @@ public class ButterflyPattern {
 			}
 			System.out.println();
 		}
-
-		for (int i = n; i >= 1; i--) {
+		for (int i = n; i >= 1; i--) { 
 			for (int j = 1; j <= 2 * n; j++) {
 				if (j <= i || j > 2 * n - i) {
 					System.out.print("*");
