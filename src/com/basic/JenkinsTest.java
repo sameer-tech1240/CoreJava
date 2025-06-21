f9595e0c6675a4bf5e357457f94ea6cda7505dd0
Sameer Siddiqui <sameersiddiqui1240@gmail.com>
2025-06-21 23:02:02 +0530

    Test class for jenkins...

---
+++ src/com/basic/JenkinsTest.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+class JenkinsTest{
+public static void main(String [] args){
+System.out.println("This is java program...");
+System.out.println("output from java program...");
+System.out.println(new Date());
+}
+}
